b8c6dea4b66b4cfc19671e4c0edc8fab4083fb1a
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-16 14:35:49 -0300

    Subindo novo projeto node.js

---
+++ node-js/1_INTRO/2-utilizando-modulo/index.js
@@ -0,0 +1,10 @@
+const { error } = require('console');
+const fs = require('fs');
+
+fs.readFile('arquivo.txt' , 'utf-8' , (err , data) => {
+    if (err){
+        console.log(err)
+    } else{
+        console.log(data)
+    }
+})
