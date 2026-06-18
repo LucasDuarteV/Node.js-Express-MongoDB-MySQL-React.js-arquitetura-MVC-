99d985fd7db4f28c751898b5ae90380116f46183
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-18 16:31:14 -0300

    terminando desafio roleplay

--- node-js/3-CORE-MODULES/npm-init/server.js
+++ node-js/3-CORE-MODULES/npm-init/server.js
@@ -1,1 +1,12 @@
-const 
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/' , (req , res) =>{
+    res.send("Servidor acessado com sucesso")
+})
+
+app.listen(port, () =>{
+    console.log("Servidor rodando na porta " + port)
+})
