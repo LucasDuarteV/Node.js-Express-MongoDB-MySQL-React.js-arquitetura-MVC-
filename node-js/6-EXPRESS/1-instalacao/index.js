bcfd3dc421ffb5141266fd5a37ea8f862a4be386
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-22 16:22:01 -0300

    Middleware

--- node-js/6-EXPRESS/1-instalacao/index.js
+++ node-js/6-EXPRESS/1-instalacao/index.js
@@ -5,6 +5,17 @@ const port = 3000
 const path = require('path')
 const basePath = path.join(__dirname , 'templates')
 
+const checkAuth = function(req , res , next){
+    if(req.authStatus){
+        console.log('Está logado, faça o login para continuar!')
+        next()
+    } else {
+        console.log("Não está logado, fala login para continuar!")
+    }
+}
+
+app.use(checkAuth)
+
 app.get('/' , (req , res) =>{
     res.sendFile(`${basePath}/index.html`)
 })
