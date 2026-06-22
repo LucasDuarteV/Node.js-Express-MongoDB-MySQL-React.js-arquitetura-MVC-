50bc3745c2a06068cc21e2558a5d0234d5d13d3a
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-22 15:30:11 -0300

    Setup inicial

--- node-js/6-EXPRESS/1-instalacao/index.js
+++ node-js/6-EXPRESS/1-instalacao/index.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/' , (req , res) =>{
+    res.send("Olá, Mundo!")
+})
+
+app.listen(port , () => {
+    console.log(`Rodando na porta: ${port}`)
+})
