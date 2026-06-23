8706acb6d7c197869109ebd9701e39a92f2c725d
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-23 10:28:58 -0300

    Lendo os dados de um POST

--- node-js/6-EXPRESS/6-param-url/index.js
+++ node-js/6-EXPRESS/6-param-url/index.js
@@ -4,6 +4,18 @@ const port = 3000
 
 const path = require('path')
 const basePath = path.join(__dirname , 'templates')
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.get('/users/add' , (req ,res) =>{
+    res.sendFile(`${basePath}/userform.html`)
+})
  
 app.get('/users/:id' , (req , res) =>{
     const id = req.params.id
@@ -13,6 +25,17 @@ app.get('/users/:id' , (req , res) =>{
 
 app.get('/' , (req , res) =>{
     res.sendFile(`${basePath}/index.html`)
+})
+
+app.post('/users/save' , (req , res) =>{
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuario é: ${name} e idade é ${age}`)
+
+    res.sendFile(`${basePath}/userform.html`)
 })
 
 app.listen(port , () => {
